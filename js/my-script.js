5a2f7ef7c3c75a52db8f231d453b07eba6c1accd
sumonkhan60 <sumonkhan60@yahoo.com>
2021-03-22 21:28:51 +0600

    code completed

---
+++ js/my-script.js
@@ -0,0 +1,56 @@
+$('#img1').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src', imgSrcValue);
+});
+
+$('#img2').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src', imgSrcValue);
+});
+
+$('#img3').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src', imgSrcValue);
+});
+
+$('#img4').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src', imgSrcValue);
+});
+
+
+
+
+
+
+
+// function createHeadingParagraphElement() {
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphELement = document.createElement('p');
+//     newHeadingElement.innerHTML = "This is a heading";
+//     newParagraphELement.innerHTML = "This is a paragraph";
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphELement);
+// }
+// document.getElementById('btn').onclick = function () {
+//   createHeadingParagraphElement();
+// };
+
+
+// $('#firstName').keyup(function () {
+//     var firstNameValue = $('#firstName').val();
+//     $('#res1').text(firstNameValue);
+// });
+//
+// $('#lastName').keyup(function () {
+//     var lastNameValue = $('#lastName').val();
+//     $('#res2').text(lastNameValue);
+// });
+//
+// $('#lastName').blur(function () {
+//     var firstNameValue = $('#firstName').val();
+//     var lastNameValue = $('#lastName').val();
+//     $('#res3').text(firstNameValue+' '+lastNameValue);
+// });
+
